d483cc1860f98c316c635dd228c94a4151d6c4ed
Isabella Serna <isabella.serna.ramirez@gmail.com>
2021-02-08 19:12:16 -0500

    Create project petgram and added some styled components
    -add story  and story list styled components
    -add image and images list styled components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ListOfCategories } from "./components/ListOfCategories";
+import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
+
+import { GlobalStyles } from "./GlobalStyles";
+
+export const App = () => (
+  <div>
+    <GlobalStyles />
+    <ListOfCategories />
+    <ListOfPhotoCards />
+  </div>
+);
